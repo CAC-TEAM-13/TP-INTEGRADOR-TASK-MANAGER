3f66255808e8bac83413fa145619b0855d2a7097
Roman Agrelo <roman.agrelo@gmail.com>
2024-04-10 16:33:46 -0300

    Roman

---
+++ tareas.js
@@ -0,0 +1,8 @@
+[
+    [63, 'Prueba 3 Tarea sin asignar', 'Prueba 3 Tarea sin asignar', '2024-03-25T18:24:58.000Z', null, '2024-04-03T19:47:03.000Z', 4, 6, 'ConradoG', null, null, null, null],
+    [61, 'Prueba 1 Tarea sin asignar', 'Prueba 1 Tarea sin asignar', '2024-03-25T18:24:16.000Z', '2024-03-26T03:00:00.000Z', '2024-03-31T03:00:00.000Z', 3, 5, 'FlorenciaR', null, null, null, null],
+    [81, 'Prueba de carga', 'otra mas', '2024-04-04T18:46:10.000Z', '2024-04-30T03:00:00.000Z', '2024-05-30T03:00:00.000Z', 1, 1, 'MatiasL', null, null, null, null],
+    [83, '123123123', '123123123', '2024-04-04T20:49:05.000Z', null, null, 1, null, null, null, null, null, null],
+    [82, 'Prueba de carga', 'Prueba de carga111111', '2024-04-04T20:25:33.000Z', '2024-04-26T03:00:00.000Z', '2024-06-21T03:00:00.000Z', 1, 2, 'FlorenciaR', null, null, null, null],
+    [62, 'Prueba 2 Tarea sin asignar', 'Prueba 2 Tarea sin asignar', '2024-03-25T18:24:42.000Z', null, null, 2, 1, 'RomanA', 41, 62, 'A la espera de respuesta de operador telefonico.', '2024-04-03T21:24:54.000Z']
+]
